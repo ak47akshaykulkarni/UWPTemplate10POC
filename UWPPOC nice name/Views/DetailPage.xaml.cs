b38120da6d9318e22c21dd6e4aedc671436c1921
Akshay Kulkarni <akshay@xamariners.com>
2018-10-10 21:53:28 +0530

    Add project files.

---
+++ UWPPOC nice name/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using UWPPOC_nice_name.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPPOC_nice_name.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
---
+++ UWPPOC nice name/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UWPPOC_nice_name.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace UWPPOC_nice_name.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
